4f9a81940db6ff2a1914a4373c21d09822bf6f31
Metreveli-A <116972081+Metreveli-A@users.noreply.github.com>
2022-11-19 12:20:47 +0400

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double num = 3.14;
+    int numb1;
+    
+    
+    cout<<"enter radius: ";
+    cin>>numb1;
+    double area = num * (numb1 * numb1);
+    cout<<area;
+    
+   
+
+    return 0;
+
+}
